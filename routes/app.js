1817e58f1208534fb8bd195a1ac5400fefd4753c
lamnaourm <mohammed.lamnaour@gmail.com>
2023-01-10 13:17:22 +0100

    routes

--- routes/app.js
+++ routes/app.js
@@ -1,6 +1,19 @@
-const express = require('express')
+const express = require("express");
+const verifyToken = require('../middleware/auth')
 const routes = express.Router();
 
+routes.post("/register", (req, res) => {});
 
+routes.post("/login", (req, res) => {});
+
+routes.get("/products",verifyToken, (req, res) => {});
+
+routes.get("/product/:name",verifyToken, (req, res) => {});
+
+routes.post("/product",verifyToken, (req, res) => {});
+
+routes.put("/product/:name",verifyToken, (req, res) => {});
+
+routes.delete("/product/:name",verifyToken, (req, res) => {});
 
 module.exports = routes;
